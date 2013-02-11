f3d0ce6d9343bdda80d0128a6b39a656aab1d044
Jerome O Castaneda <djjcast@gmail.com>
2013-02-10 20:24:47 -0600

    fix disable_toggle_minimap_on_scroll command

--- ToggleMinimapOnScroll.py
+++ ToggleMinimapOnScroll.py
@@ -8,7 +8,6 @@ ignore_count = 0
 active_view = None
 settings = sublime.load_settings("ToggleMinimapOnScroll.sublime-settings")
 toggle_minimap_on_scroll_enabled = settings.get("toggle_minimap_on_scroll_by_default", True)
-disable_toggle_minimap_on_scroll = False
 
 
 class SampleViewportPosition(sublime_plugin.TextCommand):
@@ -20,7 +19,7 @@ class SampleViewportPosition(sublime_plugin.TextCommand):
 
     def viewport_position_changed(self):
         viewport_position_changed = False
-        viewport_position_sample = self.view.viewport_position()
+        viewport_position_sample = active_view.viewport_position()
         if self.last_viewport_position_sample and viewport_position_sample[1] != self.last_viewport_position_sample[1]:
             viewport_position_changed = True
         self.last_viewport_position_sample = viewport_position_sample
@@ -29,7 +28,7 @@ class SampleViewportPosition(sublime_plugin.TextCommand):
     def toggle_minimap_for_duration(self):
         global ignore_events, ignore_count
         if not ignore_events:
-            self.view.window().run_command("toggle_minimap")
+            active_view.window().run_command("toggle_minimap")
             ignore_events = True
         else:
             ignore_count += 1
@@ -61,15 +60,12 @@ if not "viewport_position_monitor" in globals():
 
 class UntoggleMinimapOnTimeout(sublime_plugin.WindowCommand):
     def run(self):
-        global ignore_events, ignore_count, toggle_minimap_on_scroll_enabled, disable_toggle_minimap_on_scroll
+        global ignore_events, ignore_count, toggle_minimap_on_scroll_enabled
         if ignore_count:
             ignore_count -= 1
             return
         self.window.run_command("toggle_minimap")
         ignore_events = False
-        if disable_toggle_minimap_on_scroll:
-            toggle_minimap_on_scroll_enabled = False
-            disable_toggle_minimap_on_scroll = False
 
 
 class ToggleMinimapOnScroll(sublime_plugin.EventListener):
@@ -87,14 +83,14 @@ class ToggleMinimapOnScroll(sublime_plugin.EventListener):
         settings = sublime.load_settings("ToggleMinimapOnScroll.sublime-settings")
         if settings.get("toggle_minimap_on_cursor_line_changed", True) and \
            toggle_minimap_on_scroll_enabled and \
-           self.cursor_line_changed(view):
-            self.toggle_minimap_for_duration(view)
+           self.cursor_line_changed():
+            self.toggle_minimap_for_duration()
 
-    def cursor_line_changed(self, view):
+    def cursor_line_changed(self):
         cursor_line_changed = False
-        selection_begin_row = view.rowcol(view.sel()[0].begin())[0]
-        selection_end_row = view.rowcol(view.sel()[0].end())[0]
-        num_selection = len(view.sel())
+        selection_begin_row = active_view.rowcol(active_view.sel()[0].begin())[0]
+        selection_end_row = active_view.rowcol(active_view.sel()[0].end())[0]
+        num_selection = len(active_view.sel())
         if selection_begin_row != self.last_selection_begin_row or \
             selection_end_row != self.last_selection_end_row or \
             num_selection != self.last_num_selection:
@@ -111,12 +107,12 @@ class ToggleMinimapOnScroll(sublime_plugin.EventListener):
             return
         settings = sublime.load_settings("ToggleMinimapOnScroll.sublime-settings")
         if settings.get("toggle_minimap_on_view_changed", True) and toggle_minimap_on_scroll_enabled:
-            self.toggle_minimap_for_duration(view)
+            self.toggle_minimap_for_duration()
 
-    def toggle_minimap_for_duration(self, view):
+    def toggle_minimap_for_duration(self):
         global ignore_events, ignore_count
         if not ignore_events:
-            view.window().run_command("toggle_minimap")
+            active_view.window().run_command("toggle_minimap")
             ignore_events = True
         else:
             ignore_count += 1
@@ -127,8 +123,8 @@ class ToggleMinimapOnScroll(sublime_plugin.EventListener):
 
 class DisableToggleMinimapOnScroll(sublime_plugin.WindowCommand):
     def run(self):
-        global disable_toggle_minimap_on_scroll
-        disable_toggle_minimap_on_scroll = True
+        global toggle_minimap_on_scroll_enabled
+        toggle_minimap_on_scroll_enabled = False
 
     def is_enabled(self):
         return toggle_minimap_on_scroll_enabled
